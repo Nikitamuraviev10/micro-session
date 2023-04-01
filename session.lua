3bb9068f083cfa18f0193538f92c5e526996f5df
nikitamuraviev10 <nikitamuraviev10@yandex.ru>
2023-04-01 19:23:23 +0000

    Correct reopen in current tab

--- session.lua
+++ session.lua
@@ -1,4 +1,4 @@
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 local micro = import("micro")
 local config = import("micro/config")
 local buffer = import("micro/buffer")
@@ -12,18 +12,45 @@ function file_exists(name)
    return f ~= nil and io.close(f)
 end
 
+function pop_from_file(path, fn)
+
+    micro.Log("Remove from session " .. fn)
+    local f = io.open(path, "r")
+    if f == nil then
+        return
+    end
+    
+    local data = f:read("*a")
+
+    io.close(f)
+    
+    f = io.open(path, "w+")
+    for token in data:gmatch("[^\r\n]+") do
+        if token ~= fn then
+            f:write(token .. "\n")
+        end
+    end
+    io.close(f)
+end
+
+
 function onBufferOpen(bp)
     if loaded == false then
         return true
     end
 
+    local fn = cwd .. "current_session"
+
     if file_exists(bp.AbsPath) == true and bp.Path ~= "" then
-        local f = io.open(cwd .. "current_session", "a+")
+        local f = io.open(fn, "a+")
         
         if f == nil then
             return false
         end
         
+        -- Reopen file in current tab
+        pop_from_file(fn, micro.CurPane().Buf.AbsPath)
+        micro.Log("Append to session " .. bp.AbsPath)
         f:write(bp.AbsPath .. "\n")
         io.close(f)
     end
@@ -34,34 +61,23 @@ end
 function preQuit(bp)
     
     local path = cwd .. "current_session"
-    local f = io.open(path, "r")
-    --local lines = f:lines()
-    local data = f:read("*a")
-    local fn = bp:Name()
-
-    io.close(f)
-    
-    f = io.open(path, "w+")
-    
-    for token in data:gmatch("[^\r\n]+") do
-        if token ~= fn then
-            f:write(token .. "\n")
-        end
-    end
-    
-    io.close(f)
+    local fn = bp.Buf.AbsPath
+    pop_from_file(path, fn)
 
 end
 
 function init() 
     local f = io.open(cwd .. "current_session", "r")
-    local lines = f:lines()
-    
-    -- load all files from previous session
-    for line in lines do
-        micro.CurPane():NewTabCmd( {line} )
+    if f ~= nil then
+        local lines = f:lines()
+        
+        -- load all files from previous session
+        micro.CurPane():OpenCmd( {lines()} )
+        for line in lines do
+            micro.CurPane():NewTabCmd( {line} )
+        end
+        io.close(f)
     end
-    io.close(f)
     
     loaded = true
 end
